2ebac0b0853d1de7a280fad2cb86f23d084cc84b
Yasser Rabee <YasserRabee@gmail.com>
2015-10-10 20:36:08 +0200

    auth screen failure message and show-password and forget-password

--- client/auth/auth.ng.js
+++ client/auth/auth.ng.js
@@ -10,8 +10,9 @@ angular.module('pnews-controllers')
                 name: null,
                 firstName: null,
                 lastName: null,
-                showPassword: false,
-                isRegister: false
+                isShowPassword: false,
+                isRegister: false,
+                isForgotPassword: false
             };
 
             $scope.failureMessage = '';
@@ -19,28 +20,30 @@ angular.module('pnews-controllers')
             $scope.authModel = authModel;
 
             $scope.login = function () {
+                $scope.failureMessage = null;
                 var loginErr = authFactory.logIn(authModel.email, authModel.password);
-                if(loginErr){
+                if (loginErr) {
                     $scope.failureMessage = loginErr.reason;
                 }
                 console.log(loginErr);
             };
 
             $scope.register = function () {
+                $scope.failureMessage = null;
                 var names = authModel.name.split(' ');
                 authModel.firstName = names.splice(0, 1);
                 authModel.lastName = names.join(' ');
                 var registerErr = authFactory.register(authModel);
-                if(registerErr){
+                if (registerErr) {
                     $scope.failureMessage = registerErr.reason;
                 }
                 console.log(registerErr);
             };
 
-
             $scope.forgotPassword = function () {
+                $scope.failureMessage = null;
                 var forgotPasswordErr = authFactory.register(authModel.email);
-                if(forgotPasswordErr){
+                if (forgotPasswordErr) {
                     $scope.failureMessage = forgotPasswordErr.reason;
                 }
                 console.log(forgotPasswordErr);
