223a5fffab9f0cae7e5cb1cbbbff23ed1d2c17b8
Yasser Rabee <YasserRabee@gmail.com>
2015-10-10 12:35:11 +0200

    init app structure with angular in play

---
+++ client/_app.js
@@ -0,0 +1,73 @@
+angular.module('pnews-controllers', []);
+angular.module('pnews-factories', []);
+angular.module('pnews-services', []);
+
+angular.module('pnews', [
+    'angular-meteor',
+    'ngAnimate',
+    'ngSanitize',
+    'ngMessages',
+    'ngMaterial',
+    'ui.router',
+    'pnews-controllers',
+    'pnews-factories'
+])
+
+    .config([
+        '$locationProvider',
+        '$stateProvider',
+        '$urlRouterProvider',
+
+        function ($locationProvider, $stateProvider, $urlRouterProvider) {
+            $locationProvider.html5Mode(true);
+            $stateProvider
+                .state('main', {
+                    abstract: true,
+                    templateUrl: 'client/main.ng.html',
+                    controller: 'appController'
+                })
+
+                .state('main.home', {
+                    url: '/home',
+                    views: {
+                        'content@main': {
+                            templateUrl: 'client/home.ng.html'
+                        }
+                    }
+                })
+
+            ;
+
+            $urlRouterProvider.otherwise('/home');
+        }
+    ])
+
+    .run([
+        function () {
+
+            console.log('Starting pNews')
+
+        }
+
+    ])
+
+    .controller('appController', [
+        '$scope',
+        'SideNav',
+
+        function ($scope, SideNav) {
+            $scope.appName = 'pNews';
+            $scope.sideNavItems = SideNav.sideNavItems;
+            $scope.burgerIconClick = function () {
+                SideNav.toggle();
+            };
+            $scope.contentSwipeRight = function () {
+                SideNav.open();
+            };
+            $scope.contentSwipeLeft = function () {
+                SideNav.close();
+            };
+        }
+    ])
+
+;
