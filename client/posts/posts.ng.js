39be6be7b8f1d56890ee20078f544f7d52021c73
Yasser Rabee <YasserRabee@gmail.com>
2015-10-10 20:03:28 +0200

    view, list posts screens

---
+++ client/posts/posts.ng.js
@@ -0,0 +1,44 @@
+angular.module('pnews-controllers')
+
+    .controller('viewPostController', [
+        '$scope',
+        '$stateParams',
+
+        function ($scope, $stateParams) {
+            var postId = $stateParams.id;
+            // TODO: use this postId
+            $scope.post = {
+                _id: 'sdsd',
+                author: {
+                    name: 'Yasser',
+                    photo: 'img/avatar.png'
+                },
+                title: 'Series 1',
+                createdAt: new Date(),
+                content: 'Post 1',
+                hashTags: ['oo', 'pp'],
+                vote: [
+                    {
+                        voter: 'sddadm;lkjm',
+                        voteType: 'up'
+                    },
+                    {
+                        voter: 'sddadm;lkjmsdsds',
+                        voteType: 'down'
+                    }
+                ],
+                votes: {
+                    up: 15,
+                    down: 1
+                },
+                currentUserVote: 'up'
+            };
+
+            $scope.setVote = function (vote) {
+                // vote is up or down
+            }
+        }
+
+    ])
+
+;
