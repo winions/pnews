ffba9bdbf6138663787ecca3a7084ac13ce53f8b
Yasser Rabee <YasserRabee@gmail.com>
2015-10-11 11:23:58 +0200

    just save work; don't know what is here

--- client/app/pnews.ng.js
+++ client/app/pnews.ng.js
@@ -47,8 +47,14 @@ angular.module('pnews', [
             $stateProvider
                 .state('main', {
                     abstract: true,
-                    templateUrl: 'client/main.ng.html',
-                    controller: 'appController'
+                    views: {
+                        'main': {
+                            templateUrl: 'client/main.ng.html'
+                        },
+                        'side-nav@main': {
+                            templateUrl: 'client/side_nav/side_nav.ng.html'
+                        }
+                    }
                 })
 
                 .state('main.posts', {
@@ -102,10 +108,40 @@ angular.module('pnews', [
     .controller('appController', [
         '$scope',
         'SideNav',
+        '$meteor',
 
-        function ($scope, SideNav) {
+        function ($scope, SideNav, $meteor) {
             $scope.appName = 'pNews';
             $scope.sideNavItems = SideNav.sideNavItems;
+            $scope.addPost = function (series, post) {
+                console.log('$scope.addPost')
+                console.log(post)
+                postFactory.addPost(series, post);
+            };
+
+            $scope.logIn = function (email, password) {
+                console.log('$scope.login');
+                var result = authFactory.logIn(email, password);
+                console.log(result);
+            };
+
+
+            $scope.logOut = function (email, password) {
+                console.log('$scope.logOut');
+                authFactory.logOut();
+            };
+
+
+            $scope.register = function (user) {
+                console.log('$scope.register');
+                var result = authFactory.register(user);
+            };
+
+            $scope.forgotPassword = function (email) {
+                console.log('$scope.forgetPassword');
+                var result = authFactory.forgotPassword(email);
+            };
+
             $scope.burgerIconClick = function () {
                 SideNav.toggle();
             };
@@ -119,6 +155,66 @@ angular.module('pnews', [
                 SideNav.close();
                 $('#side-nav-toggle').focusout();
             };
+
+
+            $scope.sendPush = function (user) {
+                console.log('MEA trying to use push', user);
+                console.log(Push.send({
+                    from: 'System admin',
+                    title: 'News feed',
+                    text: 'This is some breaking news! Check them out!',
+                    query: {id: user.profile.gcmUserId}
+                    // query: {}
+                }));
+                // console.log('MEA current user', Meteor.user());
+
+                // user.profile.gcmToken = 'gcmToken';
+                // user.profile.gcmUserId = 'gcmUserId';
+                // console.log('MEA trying to save the user modification to db', user);
+                // $scope.allUsers.save();
+            };
+
+
+            $scope.sendPushWithToken = function (user) {
+                console.log('MEA trying to use push', user);
+                console.log(Push.send({
+                    from: 'System admin',
+                    title: 'News feed',
+                    text: 'This is some breaking news! Check them out!',
+                    token: user.profile.gcmToken
+                    // query: {}
+                }));
+                // console.log('MEA current user', Meteor.user());
+
+                // user.profile.gcmToken = 'gcmToken';
+                // user.profile.gcmUserId = 'gcmUserId';
+                // console.log('MEA trying to save the user modification to db', user);
+                // $scope.allUsers.save();
+            };
+
+            Push.allow({
+                send: function (userId, notification) {
+                    return true; // Allow all users to send
+                }
+            });
+
+            var gcmToken = null, gcmUserId = null;
+            Push.addListener('token', function (token) {
+                console.log('MEA Token: ' + JSON.stringify(token));
+                gcmToken = JSON.stringify(token);
+                gcmUserId = Push.id();
+                alert('MEA Token: ' + JSON.stringify(token));
+                var currentUser = Meteor.user();
+                if (currentUser !== null) {
+                    currentUser.profile.gcmToken = gcmToken;
+                    currentUser.profile.gcmUserId = gcmUserId;
+                    Meteor.users.update({_id: Meteor.userId()}, {$set: {'profile': currentUser.profile}});
+                    console.log('MEA trying to update current User model', gcmToken, gcmUserId, currentUser);
+                }
+            });
+
+            $scope.allUsers = $meteor.collection(Meteor.users, false).subscribe('users');
+            console.log('MEA all users', $scope.allUsers);
         }
     ])
 
